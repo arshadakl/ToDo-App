c728a2226415a40f43d1dd6f8afa49e4c9bf1994
arshadakl <arshadayanikkal@gmail.com>
2024-01-04 21:41:48 +0530

    design added

--- src/Tab.jsx
+++ src/Tab.jsx
@@ -2,99 +2,71 @@ import React from 'react'
 
 function Tab() {
     return (
+        <div className='mainDiv'>
             <div className="container" id="container">
 
                 <aside>
                     <div>
-                        <input type="text" placeholder="Search" />
+                    <div>
+                        {/* <input type="text" placeholder="Search" /> */}
+                        <h1 className='ToDOLogo'>ToDo</h1>
                         <br />
                         <button className="active" id="sidebar-btn">
-                            Discover
+                        Home
                         </button>
-                        <button id="sidebar-btn">Arcade</button>
+                        <button  id="sidebar-btn">
+                        Completed
+                        </button>
+                        <button id="sidebar-btn">Pending</button>
                     </div>
-                    {/* <div className="profile">
-                        <img
-                            src="https://pbs.twimg.com/profile_images/994592419705274369/RLplF55e.jpg"
-                            alt="Profile Pic"
-                            className="profile--pic"
-                        />
-                        <h1>Jimmy Donaldson</h1>
-                    </div> */}
+                    
+                    </div>
                 </aside>
                 <div className="content">
                     <div className="ideas">
                         <div className="idea">
-                            <p>MASTER YOUR MAC</p>
+                            {/* <p>achievement</p> */}
                             <h1>
-                                Make a great
-                                <br /> handoff
+                            Completed Tasks
+                                <br /><span className='taskCount'>0</span>
                             </h1>
-                            <p>Work seamlessly across devices</p>
+                            {/* <p>0</p> */}
                         </div>
                         <div className="idea">
-                            <p>GET STARTED</p>
-                            <h1>Apps optimised for mac with M1</h1>
-                            <p>Work seamlessly across devices</p>
+                            {/* <p>sd</p> */}
+                            <h1>Pending</h1>
+                            <p>sd</p>
                         </div>
                     </div>
                     <section className="updates">
-                        <h1>Updates</h1>
-                        <div className="update update-1">
+                        <h1>All Task</h1>
+                        <div className="update ">
                             <div className="updt-left">
-                                <img
+                                {/* <img
                                     src="https://img.icons8.com/color/452/microsoft-powerpoint-2019.png"
                                     alt="powerpoint icon"
-                                />
-                                <h1>Microsoft Powerpoint</h1>
+                                /> */}
+                                <h1>test</h1>
                             </div>
                             <div className="buttons">
                                 <a href="https://bit.ly/3gdGvT8" target="_warning">
-                                    <button>Update</button>
+                                    <button>Edit</button>
                                 </a>
                             </div>
                         </div>
                         
-                    </section>
-                    {/* <section className="trending">
-                        <h1>Trending</h1>
-                        <div className="grid">
-                            <span className="grid__app">
-                                <h1>Kindle</h1>
-                                <button>GET</button>
-                            </span>
-                            <span className="grid__app">
-                                <h1>Slac</h1>
-                                <button>GET</button>
-                            </span>
-                            <span className="grid__app">
-                                <h1>Magnet</h1>
-                                <button>₹699</button>
-                            </span>
-                            <span className="grid__app">
-                                <h1>CotEditor</h1>
-                                <button>GET</button>
-                            </span>
-                            <span className="grid__app margin">
-                                <h1>Twitter</h1>
-                                <button>GET</button>
-                            </span>
-                            <span className="grid__app margin">
-                                <h1>Notability</h1>
-                                <button>₹799</button>
-                            </span>
-                            <span className="grid__app margin">
-                                <h1>NDTV</h1>
-                                <button>GET</button>
-                            </span>
-                            <span className="grid__app margin">
-                                <h1>Rummy</h1>
-                                <button>₹179</button>
-                            </span>
+                        <div className="addSection">
+                            <input placeholder='Add a Task' className='addInput' type="text" />
+                            <button className="fa-solid fa-plus" onClick={()=>{
+                                console.log("clicked")
+                            }}></button>
                         </div>
-                    </section> */}
+                        
+                    </section>
+                   
                    
                 </div>
+            </div>
             </div>
 
        
